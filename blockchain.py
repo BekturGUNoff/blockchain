5c5ec1d7d2e4a4ebb240ac43592fcd2edfafa791
BekturGUNoff <103578281+BekturGUNoff@users.noreply.github.com>
2023-09-25 10:59:42 +0600

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,134 +1,161 @@
 import hashlib
 import json
 from time import time
-from flask import Flask, request, jsonify
-
-# Define the Flask app
-app = Flask(__name__)
 
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.transactions_pool = []
 
         # Create the genesis block
-        self.new_block(previous_hash="1")
+        self.new_block(previous_hash="1", proof=100)
 
-    def new_block(self, previous_hash):
+    def new_block(self, proof, previous_hash=None):
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
             'transactions': self.current_transactions,
+            'proof': proof,
             'previous_hash': previous_hash or self.hash(self.chain[-1]) if self.chain else None,
         }
 
-        # Calculate the Merkle tree root for transactions in the block
-        block['merkle_root'] = self.calculate_merkle_root(block['transactions'])
-
-        # Reset the current list of transactions
         self.current_transactions = []
-
         self.chain.append(block)
         return block
 
-    @staticmethod
-    def hash(block):
-        # Hash a block using SHA-256
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
-
     def new_transaction(self, sender, recipient, amount):
-        # Create a new transaction and add it to the list of transactions
         transaction = {
             'sender': sender,
             'recipient': recipient,
             'amount': amount,
         }
-        self.current_transactions.append(transaction)
-        return self.last_block['index'] + 1  # Return the index of the block that will include this transaction
-
-    def calculate_merkle_root(self, transactions):
-        # Create a Merkle tree and return its root hash
-        if not transactions:
-            return None
-
-        if len(transactions) == 1:
-            return self.hash(transactions[0])
-
-        # Create a list of transaction hashes
-        transaction_hashes = [self.hash(tx) for tx in transactions]
-
-        # Recursively build the Merkle tree
-        while len(transaction_hashes) > 1:
-            new_hashes = []
-            for i in range(0, len(transaction_hashes), 2):
-                if i + 1 < len(transaction_hashes):
-                    combined_hash = self.hash(transaction_hashes[i] + transaction_hashes[i + 1])
-                else:
-                    combined_hash = self.hash(transaction_hashes[i])
-                new_hashes.append(combined_hash)
-            transaction_hashes = new_hashes
-
-        return transaction_hashes[0]
-
-    def proof_of_work(self, last_hash):
-        # Implement a basic Proof of Work algorithm to find a nonce that, when hashed, produces a hash with leading zeros
-        proof = 0
-        while self.valid_proof(last_hash, proof) is False:
-            proof += 1
-        return proof
+        self.transactions_pool.append(transaction)
+        return self.last_block['index'] + 1
 
     @staticmethod
-    def valid_proof(last_hash, proof):
-        # Check if the hash of the last hash and current proof contains leading zeros (adjust difficulty as needed)
-        guess = f'{last_hash}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"  # Adjust the number of leading zeros as per network difficulty
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
-# Create a new blockchain instance
+    @property
+    def last_block(self):
+        return self.chain[-1] if self.chain else None
+
+    def mine_block(self, miner_address):
+        if not self.transactions_pool:
+            return None  # No transactions to mine
+
+        # Proof of work calculation (simplified for demonstration)
+        proof = 1
+        while not self.valid_proof(proof):
+            proof += 1
+
+        # Create a new block
+        previous_hash = self.hash(self.last_block)
+        block = self.new_block(proof, previous_hash)
+
+        # Add transactions from the pool to the new block
+        block['transactions'] = self.transactions_pool
+        self.transactions_pool = []
+
+        return block
+
+    def valid_proof(self, proof):
+        # Simplified proof-of-work validation (requires first 4 characters to be zeros)
+        guess = f'{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    def is_valid_transaction(self, transaction):
+        # Simplified transaction validation (checking sender has enough balance)
+        sender_balance = self.get_balance(transaction['sender'])
+        return sender_balance >= transaction['amount']
+
+    def get_balance(self, address):
+        # Calculate the balance of an address based on the blockchain's history
+        balance = 0
+        for block in self.chain:
+            for transaction in block['transactions']:
+                if transaction['sender'] == address:
+                    balance -= transaction['amount']
+                if transaction['recipient'] == address:
+                    balance += transaction['amount']
+        return balance
+
+# Initialize the blockchain
 blockchain = Blockchain()
 
-@app.route('/add_transaction', methods=['POST'])
-def add_transaction():
-    data = request.get_json()
-    sender = data['sender']
-    recipient = data['recipient']
-    amount = data['amount']
+# Example transactions
+blockchain.new_transaction("Alice", "Bob", 5)
+blockchain.new_transaction("Bob", "Charlie", 3)
+blockchain.new_transaction("Charlie", "Alice", 2)
 
-    # Add the transaction to the blockchain
-    index = blockchain.new_transaction(sender, recipient, amount)
+# Mining a new block
+miner_address = "Miner1"
+new_block = blockchain.mine_block(miner_address)
 
-    response = {'message': f'Transaction will be included in block {index}'}
-    return jsonify(response), 200
+if new_block:
+    print("Block mined successfully.")
+    print("Chain:", json.dumps(blockchain.chain, indent=4))
+else:
+    print("No transactions to mine.")
+class BlockchainCLI:
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
 
-@app.route('/mine_block', methods=['GET'])
-def mine_block():
-    # Get the last block's hash
-    last_block = blockchain.last_block
-    last_hash = blockchain.hash(last_block)
+    def display_menu(self):
+        print("Blockchain CLI Menu:")
+        print("1. Create Transaction")
+        print("2. Mine a Block")
+        print("3. Check Balance")
+        print("4. Display Chain")
+        print("5. Quit")
 
-    # Find a valid proof for the new block
-    proof = blockchain.proof_of_work(last_hash)
+    def create_transaction(self):
+        sender = input("Enter sender: ")
+        recipient = input("Enter recipient: ")
+        amount = float(input("Enter amount: "))
+        if self.blockchain.new_transaction(sender, recipient, amount):
+            print("Transaction added to the pool.")
+        else:
+            print("Invalid transaction. Insufficient balance.")
 
-    # Create the new block
-    previous_hash = last_hash
-    block = blockchain.new_block(previous_hash)
-    block['proof'] = proof  # Include the proof in the block
+    def mine_block(self):
+        miner_address = input("Enter miner's address: ")
+        new_block = self.blockchain.mine_block(miner_address)
+        if new_block:
+            print("Block mined successfully.")
+        else:
+            print("No transactions to mine.")
 
-    response = {
-        'message': 'New block mined!',
-        'block': block
-    }
-    return jsonify(response), 200
+    def check_balance(self):
+        address = input("Enter address to check balance: ")
+        balance = self.blockchain.get_balance(address)
+        print(f"Balance of {address}: {balance} units")
 
-@app.route('/chain', methods=['GET'])
-def get_chain():
-    # Return the entire blockchain
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
-    }
-    return jsonify(response), 200
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = input("Enter your choice: ")
+            if choice == "1":
+                self.create_transaction()
+            elif choice == "2":
+                self.mine_block()
+            elif choice == "3":
+                self.check_balance()
+            elif choice == "4":
+                print(json.dumps(self.blockchain.chain, indent=4))
+            elif choice == "5":
+                break
+            else:
+                print("Invalid choice. Please try again.")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# Initialize the blockchain
+blockchain = Blockchain()
+
+# Initialize the CLI interface
+cli = BlockchainCLI(blockchain)
+
+# Run the CLI
+cli.run()
